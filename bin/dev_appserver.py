ab68279ab40baca52d0a203bae3883715e68e9de
Google Cloud SDK Mirror <cloudsdk.mirror@gmail.com>
2017-12-08 11:55:08 +0900

    0.9.83

---
+++ bin/dev_appserver.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+
+"""A convenience wrapper for starting dev_appserver for appengine for python."""
+
+import os
+
+import bootstrapping.bootstrapping as bootstrapping
+
+from googlecloudsdk.core.updater import update_manager
+
+
+def main():
+  """Launches dev_appserver.py."""
+  update_manager.UpdateManager.EnsureInstalledAndRestart(
+      ['app-engine-java', 'app-engine-python', 'app-engine-php'],
+      command=__file__)
+
+  args = [
+      '--skip_sdk_update_check=True'
+  ]
+
+  bootstrapping.ExecutePythonTool(
+      os.path.join('platform', 'google_appengine'), 'dev_appserver.py', *args)
+
+
+if __name__ == '__main__':
+  bootstrapping.CommandStart('dev_appserver', component_id='app')
+  bootstrapping.CheckUpdates()
+  main()
